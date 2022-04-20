3582112712f318aafb44f64d30f371da22f6c4d9
Daniel Jacobi <daniel@questmaster.de>
2022-04-20 23:42:11 +0200

    Generate bindings for esp32-camera code works build fails due to dependency to esp-idf that is currently not fulfilled

--- libespcam-sys/README.md
+++ libespcam-sys/README.md
@@ -1,11 +1,8 @@
 Bindgen call
 ============
 
-Used esp32-camera relese 2.0.0
-Still some includes and definitions missing...
+Used esp32-camera release 2.0.0
+works...
 
-bindgen.exe .\esp32-camera\driver\include\esp_camera.h -o .\src\bindings.rs -- -I.\.embuild\platformio\packages\framework-espidf\components\esp_common\inc
-lude\ -I.\.embuild\platformio\packages\toolchain-xtensa-esp32\xtensa-esp32-elf\include\ -I.\.embuild\platformio\packages\framework-espidf\components\driver\include\ -I.\.embuild\platformio\packages\framework-espidf\compon
-ents\esp_system\include\ -I.\.embuild\platformio\packages\framework-espidf\components\soc\esp32\include\ -I.\.embuild\platformio\packages\framework-espidf\components\hal\include\ -I.\.embuild\platformio\packages\framework
--espidf\components\xtensa\include\ -I. -I.\.embuild\platformio\packages\framework-espidf\components\soc\include\ -DCONFIG_IDF_TARGET_ESP32 -I.\.embuild\platformio\packages\framework-espidf\components\esp32\include\
+bindgen.exe --no-rustfmt-bindings .\esp32-camera\driver\include\esp_camera.h -o .\src\bindings.rs -- -I.\.embuild\platformio\packages\framework-espidf\components\esp_common\include\ -I.\.embuild\platformio\packages\toolchain-xtensa-esp32\xtensa-esp32-elf\include\ -I.\.embuild\platformio\packages\framework-espidf\components\driver\include\ -I.\.embuild\platformio\packages\framework-espidf\components\esp_system\include\ -I.\.embuild\platformio\packages\framework-espidf\components\soc\esp32\include\ -I.\.embuild\platformio\packages\framework-espidf\components\hal\include\ -I.\.embuild\platformio\packages\framework-espidf\components\xtensa\include\ -I. -I.\.embuild\platformio\packages\framework-espidf\components\soc\include\ -DCONFIG_IDF_TARGET_ESP32 -I.\.embuild\platformio\packages\framework-espidf\components\esp32\include\ -I.\.embuild\platformio\packages\framework-espidf\components\esp_rom\include\ -I.\esp32-camera\conversions\include\ -I.\esp32-camera\driver\include\
 
--- libespcam-sys/build.rs
+++ libespcam-sys/build.rs
@@ -1,8 +1,54 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    pkg_config::Config::new()
-        .atleast_version("1.2")
-        .probe("z")
-        .unwrap();
+    // Tell cargo to tell rustc to link the system nvpair of zfs
+    // shared library.
+    println!("cargo:rustc-link-lib=esp32-camera");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header(r#".\esp32-camera\driver\include\esp_camera.h"#)
+        .allowlist_type(r#"(\w*camera_\w*)"#)
+        .allowlist_function(r#"(\w*esp_camera\w*)"#)
+        .blocklist_file(r#".\.embuild\*"#)
+        .clang_args(vec![r#"-I.\.embuild\platformio\packages\framework-espidf\components\esp_common\include\"#,
+            r#"-I.\.embuild\platformio\packages\toolchain-xtensa-esp32\xtensa-esp32-elf\include\"#,
+            r#"-I.\.embuild\platformio\packages\framework-espidf\components\driver\include\"#,
+            r#"-I.\.embuild\platformio\packages\framework-espidf\components\esp_system\include\"#,
+            r#"-I.\.embuild\platformio\packages\framework-espidf\components\soc\esp32\include\"#,
+            r#"-I.\.embuild\platformio\packages\framework-espidf\components\hal\include\"#,
+            r#"-I.\.embuild\platformio\packages\framework-espidf\components\xtensa\include\"#,
+            "-I.",
+            r#"-I.\.embuild\platformio\packages\framework-espidf\components\soc\include\"#,
+            "-DCONFIG_IDF_TARGET_ESP32",
+            r#"-I.\.embuild\platformio\packages\framework-espidf\components\esp32\include\"#,
+            r#"-I.\.embuild\platformio\packages\framework-espidf\components\esp_rom\include\"#,
+            r#"-I.\esp32-camera\conversions\include\"#,
+            r#"-I.\esp32-camera\driver\include\"#])
+        // Disable rustfmt, as it is not available on the esp toolchain
+        .rustfmt_bindings(false)
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from("./src");
+//    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+
+//    pkg_config::Config::new()
+//        .atleast_version("1.2")
+//        .probe("esp-idf")
+//        .unwrap();
     let src = [
         // common files
         "esp32-camera/driver/esp_camera.c",
@@ -10,15 +56,15 @@ fn main() {
         "esp32-camera/driver/sccb.c",
         "esp32-camera/driver/sensor.c",
         "esp32-camera/sensors/ov2640.c",
-        "esp32-camera/sensors/ov3660.c",
-        "esp32-camera/sensors/ov5640.c",
-        "esp32-camera/sensors/ov7725.c",
-        "esp32-camera/sensors/ov7670.c",
-        "esp32-camera/sensors/nt99141.c",
-        "esp32-camera/sensors/gc0308.c",
-        "esp32-camera/sensors/gc2145.c",
-        "esp32-camera/sensors/gc032a.c",
-        "esp32-camera/sensors/bf3005.c",
+//        "esp32-camera/sensors/ov3660.c",
+//        "esp32-camera/sensors/ov5640.c",
+//        "esp32-camera/sensors/ov7725.c",
+//        "esp32-camera/sensors/ov7670.c",
+//        "esp32-camera/sensors/nt99141.c",
+//        "esp32-camera/sensors/gc0308.c",
+//        "esp32-camera/sensors/gc2145.c",
+//        "esp32-camera/sensors/gc032a.c",
+//        "esp32-camera/sensors/bf3005.c",
         "esp32-camera/conversions/yuv.c",
         "esp32-camera/conversions/to_jpg.cpp",
         "esp32-camera/conversions/to_bmp.c",
@@ -31,13 +77,18 @@ fn main() {
     ];
     let inc = [
         "esp32-camera/driver/include",
+        "esp32-camera/driver/private_include",
         "esp32-camera/conversions/include",
-
+//        ".embuild/platformio/packages/framework-espidf/components/freertos/include",
+//        ".embuild/platformio/packages/framework-espidf/components/heap/include",
     ];
     let mut builder = cc::Build::new();
     let build = builder
         .files(src.iter())
         .includes(inc.iter())
+        .target("xtensa-esp32-elf")
+        .shared_flag(true)
+        .compiler(PathBuf::from("./.embuild/platformio/packages/toolchain-xtensa-esp32/bin/xtensa-esp32-elf-cc.exe"))
         .flag("-Wno-unused-parameter");
 //        .define("USE_ZLIB", None);
     build.compile("esp32-camera");
